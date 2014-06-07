eaaf3235a05f0abcfa9c662b4f537feb361090eb
gavard.e@gmail.com <gavard.e@gmail.com@f01e0e1a-e0ec-a4ba-7229-b15e5b2af39f>
2014-06-07 21:46:21 +0000

    Premier commit du projet avec Spring

---
+++ tvseriestracker/src/src/controller/IndexController.java
@@ -0,0 +1,21 @@
+package src.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+@org.springframework.stereotype.Controller
+@RequestMapping("/index.html")
+public class IndexController implements Controller {
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView handleRequest(HttpServletRequest request,HttpServletResponse response) throws Exception {
+		ModelAndView modelAndView = new ModelAndView("/showMessage");
+		modelAndView.addObject("message","salut le monde youhou");
+		return modelAndView;
+	}
+
+}
